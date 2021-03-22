4800576461ada545e4146bda9844fc42fedc6e9e
Liam Mcglynn <liammcglynn@mcglynn14webdev.com>
2021-03-22 14:54:10 +0000

    Added js/script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function changeParagraph()
+{
+    document.getElementById("p1").innerHTML="my name is Liam";
+}
+
+function changeParaColor()
+{
+    document.getElementById("p1").style.color="blue";
+}
