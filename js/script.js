1c84639a81997bac5653f9528a6f4c2a3801b4c2
liam mcglynn <mcglynn14@outlook.com>
2021-08-28 00:45:05 +0100

    Moved to nqlevel5 folder

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function changeParagraph()
-{
-    document.getElementById("p1").innerHTML="my name is Liam";
-}
-
-function changeParaColor()
-{
-    document.getElementById("p1").style.color="blue";
-}
