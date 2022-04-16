6dd8115ed7927f106bcda97da611d35cf4bd6021
liam Mcglynn <liammcglynn@liams-MacBook-Pro.local>
2022-04-16 21:42:33 +0100

    Added files and folders

---
+++ mobilenavtask/js/script.js
@@ -0,0 +1,9 @@
+
+var mainNav = document.getElementById("js-menu");
+
+
+var navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener('click', function(){
+    mainNav.classList.toggle("active");
+});
