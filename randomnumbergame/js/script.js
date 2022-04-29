7fc38bbac575a4506682944e1d5dbfe2b42b2a26
liam Mcglynn <liammcglynn@liams-MacBook-Pro.local>
2022-04-29 22:10:53 +0100

    Added folder and files

---
+++ randomnumbergame/js/script.js
@@ -0,0 +1,11 @@
+var usernum = parseInt(document.getElementById("usernuminput").value);
+
+var random = Math.floor((Math.random() * 12) + 1);
+
+function randomnum(
+    var random = Math.floor((Math.random() * 12) + 1);
+
+    document.write(getElementById)
+    
+
+)
