b306cb2e5a47c80a40e68fed196cc0505b9cda92
mcglynn14 <mcglynn14@outlook.com>
2022-11-30 09:19:24 +0000

    Added sdsmallstand folder with new folders and files

---
+++ hndweb/sdsmallstand/practical/domexercises/js/domscripts.js
@@ -0,0 +1,25 @@
+// domexercises.html
+
+function setUp(){
+    document.getElementById("innerHTML").innerHTML = "It's cold outside!!!";
+}
+
+function Window(){
+    document.getElementById("window").innerHTML = "Close the window!!!";
+}
+
+// Class code.html
+function classCode(){
+
+    document.getElementById("heading").innerHTML = "My class code is: HNDWEBEN-F221A-L";
+
+    document.getElementById("heading").style.color = "red"
+
+    document.getElementById("heading").style.fontFamily = "papyrus";
+
+    document.getElementById("heading").style.backgroundColor = "black";
+
+    document.getElementById("heading").style.fontSize = "48px";
+
+    document.getElementById("body").style.backgroundColor = "black";
+}
